8fd97304a1db6479fee2e72b2790d68af2bc13b4
Francesco Piscani <fpiscani@gmail.com>
2024-05-16 15:07:21 -0400

    add Map and Flatlist functionality

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+const days = [...Array(31).keys()].map((val, index) => index + 1);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={3}
+        renderItem={({ item }) => (
+          <View style={styles.box}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +27,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: {
+    gap: 10,
+  },
+  box: {
+    backgroundColor: "#F9EDE3",
+
+    aspectRatio: 1,
+    width: 100,
+    //height: 100,
     justifyContent: "center",
+    alignItems: "center",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#9b4521",
+    borderRadius: 20,
+    padding: 10,
+  },
+  text: {
+    color: "#9b4521",
+    fontSize: 50,
   },
 });
