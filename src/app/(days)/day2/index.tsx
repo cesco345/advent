d2cc81f1618a9b1e7840d64e1ac2d7b1a1f7f7e0
Francesco Piscani <fpiscani@gmail.com>
2024-06-02 00:03:40 -0400

    add gesture handler functionality

--- src/app/(days)/day2/index.tsx
+++ src/app/(days)/day2/index.tsx
@@ -5,10 +5,11 @@ import { Link, Stack } from "expo-router";
 const DayDetailsScreen = () => {
   return (
     <View>
-      <Stack.Screen options={{ title: "Day 2 Onboarding" }} />
+      <Stack.Screen options={{ title: "Day 2: Onboarding" }} />
       <Text>Day Details Screen</Text>
+
       <Link href="/day2/onboarding" asChild>
-        <Button title="Go To Onboarding" onPress={() => {}} />
+        <Button title="Go to onboarding" />
       </Link>
     </View>
   );
